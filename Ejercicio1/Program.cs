fe1652dc9009755e9161c312ebebf4e565ba57c0
XXnikoDarkXX <nicocheto1@gmail.com>
2020-03-12 18:48:27 +0100

    dfdf
    dfdf

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -7,25 +7,49 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
-            int tamaño;
-            Console.WriteLine("Cuantos alumnos hay");
-            tamaño = Convert.ToInt32(Console.ReadLine());
-            
-            String[] nombre = new string[tamaño];
-            float[] notas = new float[tamaño];
-            for (int i = 0; i < nombre.Length; i++)
-            {
-                Console.WriteLine("Dime el nombre del alumno"+i);
-                nombre[i] = Console.ReadLine();
-                Console.WriteLine("Los nombres son: "+ nombre[i]);
+            Console.WriteLine("¿Cuantos alumnos hay en clase?: ");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Los alumnos que hay son: " + cantidad);
+            String nombre;
+            float nota;
+            String[] alumnos = new string[cantidad];
+            float[] notasAlumnos = new float[cantidad];
 
+            for (int i = 0; i < cantidad; i++)
+            {
+
+                Console.WriteLine("Introduce el nombre del alumno: ");
+                nombre = Console.ReadLine();
+                alumnos[i] = nombre;
+
+                do
+                {
+                    Console.WriteLine("Introduce la nota del alumno: ");
+                    nota = float.Parse(Console.ReadLine());
+
+                } while (nota < 0 || nota > 10);
+                notasAlumnos[i] = nota;
 
             }
-            for (int i = 0; i < notas.Length; i++)
+
+            String mejorAlumno = "";
+            float mejorNota = 0;
+
+            for (int k = 0; k < cantidad; k++)
             {
-                Console.WriteLine("Dime las notas del alumno "+i);
-                notas = Convert.ToSingle();
+
+                if (notasAlumnos[k] > mejorNota)
+                {
+
+                    mejorNota = notasAlumnos[k];
+                    mejorAlumno = alumnos[k];
+
+                }
             }
+
+            Console.WriteLine("Este es el mejor alumno: " + mejorAlumno);
+            Console.WriteLine("Esta es su nota: " + notasAlumnos.Max());
+            Console.WriteLine("Esta es la media del curso: " + (notasAlumnos.Sum()) / cantidad);
         }
     }
 }
