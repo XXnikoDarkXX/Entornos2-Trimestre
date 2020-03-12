f969ed5de500113b182efd85b95814b0334dfcbb
XXnikoDarkXX <nicocheto1@gmail.com>
2020-03-12 18:45:41 +0100

    primer ejercicio
    erer

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+namespace Ejercicio1
+  
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamaño;
+            Console.WriteLine("Cuantos alumnos hay");
+            tamaño = Convert.ToInt32(Console.ReadLine());
+            
+            String[] nombre = new string[tamaño];
+            float[] notas = new float[tamaño];
+            for (int i = 0; i < nombre.Length; i++)
+            {
+                Console.WriteLine("Dime el nombre del alumno"+i);
+                nombre[i] = Console.ReadLine();
+                Console.WriteLine("Los nombres son: "+ nombre[i]);
+
+
+            }
+            for (int i = 0; i < notas.Length; i++)
+            {
+                Console.WriteLine("Dime las notas del alumno "+i);
+                notas = Convert.ToSingle();
+            }
+        }
+    }
+}
