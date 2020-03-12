64b4c33be9e51c0c26d8dedc60170d0838e556ac
XXnikoDarkXX <nicocheto1@gmail.com>
2020-03-12 18:59:13 +0100

    2ºEjercicio Condiciones Compuestas

---
+++ CondicionesCompuestas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CondicionesCompuestas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              String linea;
+            String mes;
+            Console.Write("Ingrese las 3 primeras letras del mes");
+            linea = Console.ReadLine();
+            mes = lineas
+           mes= mes.ToLower();
+            if (mes == "oct" || mes == "nov" || mes == "dic"||mes == "ene" || mes == "feb" 
+                || mes == "mar"||mes == "abr" || mes == "may" || mes == "jun")
+            {
+                if (mes == "oct" || mes == "nov" || mes == "dic"){
+                    Console.WriteLine("Pertenece al primer trimestre");
+                }else if(mes == "ene" || mes == "feb" || mes == "mar"){
+                    Console.WriteLine("Pertenece al segundo trimestre");
+                }else if (mes == "abr" || mes == "may" || mes == "jun")
+                {
+                    Console.WriteLine("Pertenece al tercer trimestre");
+                }
+            } else{ Console.WriteLine("Mes incorrecto"); }
+        }
+    }
+}
